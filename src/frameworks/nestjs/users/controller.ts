5d9438f23a19fbaae30ae214034d227db9713dcc
frostice482 <frostice48260@gmail.com>
2026-05-21 21:25:44 +0700

    fix nestjs

--- src/frameworks/nestjs/users/controller.ts
+++ src/frameworks/nestjs/users/controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, Query } from '@nestjs/common'
+import { Body, Controller, Get, ParseIntPipe, Post, Query } from '@nestjs/common'
 import UserProvider from './provider';
 import type { UserCreateManyInput } from '../../../prisma/models';
 
@@ -7,8 +7,7 @@ export default class UserController {
 	constructor(private svc: UserProvider) {}
 
 	@Get('/users')
-	async getUsers(@Query('limit') limit?: number) {
-		console.log(limit)
+	async getUsers(@Query('limit', ParseIntPipe) limit?: number) {
 		return this.svc.getUsers(limit)
 	}
 
